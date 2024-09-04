59fe7548830a6a675e9e61e1739eee05fdbf4537
Brian Goldman <leonel89011@gmail.com>
2024-09-04 10:46:02 -0300

    Basic MySQL info

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"github.com/brianleogoldman/goshop/cmd/api"
+	database "github.com/brianleogoldman/goshop/db"
+	"github.com/go-sql-driver/mysql"
 	"log"
 )
 
 func main() {
+	db, err := database.NewMySQLStorage(mysql.Config{
+		User:                 "root",
+		Passwd:               "pass123",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "goshop",
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
