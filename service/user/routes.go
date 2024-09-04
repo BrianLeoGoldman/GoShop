b3880221aaa60fcca9a8ee7b29fbd86787c0dd8e
Brian Goldman <leonel89011@gmail.com>
2024-09-04 16:25:45 -0300

    New User registration implmented

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,19 +1,21 @@
 package user
 
 import (
+	"fmt"
 	"github.com/brianleogoldman/goshop/types"
 	"github.com/brianleogoldman/goshop/utils"
 	"github.com/gorilla/mux"
 	"net/http"
+	"time"
 )
 
 // Handler struct is a service
 type Handler struct {
-	store *types.UserStore
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -30,4 +32,21 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	if err := utils.ParseJSON(r, payload); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
 	}
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  payload.Password,
+		CreatedAt: time.Now(),
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
--- service/user/store.go
+++ service/user/store.go
@@ -34,6 +34,14 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	return user, nil
 }
 
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	return nil, nil
+}
+
+func (s *Store) CreateUser(user types.User) error {
+	return nil
+}
+
 func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
 	user := new(types.User)
 	err := rows.Scan(
